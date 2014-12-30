26ed1022a34fd28cf7847c15e6726e2f37a34bab
Patrick Sullivan <patdsully88@gmail.com>
2014-12-29 19:10:40 -0600

    some fixes to do per user.

--- app/app.js
+++ app/app.js
@@ -6,35 +6,61 @@
 var app = angular.module("sampleApp", ["firebase"]);
 
 app.factory("Auth", ["$firebaseAuth", function($firebaseAuth) {
-    var ref = new Firebase("https://ChkBook.firebaseio.com/");
+    var ref = new Firebase("https://pdsullivaftestapp.firebaseio.com/");
     return $firebaseAuth(ref);
 }]);
 
 // and use it in our controller
 app.controller("SampleController", ["$scope", "Auth","$firebase", function($scope, Auth, $firebase) {
+
     $scope.auth = Auth;
+
+    //$scope.createUserData = {
+    //    email: "",
+    //    password: ""
+    //};
+    //
+    //$scope.loginData = {
+    //    email: "",
+    //    password: ""
+    //};
 
     $scope.user = $scope.auth.$getAuth();
 
-    $scope.ref = new Firebase("https://ChkBook.firebaseio.com/");
-    var usersRef = $scope.ref.child("users");
+    $scope.ref = new Firebase("https://pdsullivaftestapp.firebaseio.com/");
 
-    var userSync = $firebase(usersRef);
+    var usersRef = null;
 
-    // if ref points to a data collection
-    $scope.list = userSync.$asArray();
+    var clearPopulateData = function(){
 
-    $scope.createUserData = {
-        email: "",
-        password: ""
+        $scope.createUserData = {
+            email: "",
+            password: ""
+        };
+
+        $scope.loginData = {
+            email: "",
+            password: ""
+        };
+    }
+
+    var loadUserData = function(){
+        if($scope.user && $scope.user.uid){
+            var usersRef = $scope.ref.child("users" + $scope.user.uid);
+
+            var userSync = $firebase(usersRef);
+
+            // if ref points to a data collection
+            $scope.list = userSync.$asArray();
+
+        }
     };
-    $scope.loginData = {
-        email: "",
-        password: ""
-    };
+
+
 
     $scope.addItem = function(text){
         $scope.list.$add(text);
+        $scope.addItemText = null;
     }
 
     $scope.createUser = function(user){
@@ -42,16 +68,21 @@ app.controller("SampleController", ["$scope", "Auth","$firebase", function($scop
             .then(function(user){
                 // do things if success
                 $scope.user = $scope.auth.$getAuth();
+                alert('You Are Registered Now Log In.');
+                clearPopulateData();
 
             }, function(error){
                 // do things if failure
                 console.log(error);
-            }); ;
+                alert('Error Registering User');
+            });
     }
 
     $scope.logOut = function(){
+
         $scope.auth.$unauth();
         $scope.user = $scope.auth.$getAuth();
+        clearPopulateData();
     }
 
     $scope.authUser = function(email,password) {
@@ -62,13 +93,18 @@ app.controller("SampleController", ["$scope", "Auth","$firebase", function($scop
             .then(function(user){
                 // do things if success
                 $scope.user = $scope.auth.$getAuth();
+                loadUserData();
+                clearPopulateData();
             }, function(error){
                 // do things if failure
                 console.log(error);
-            }); ;
+                alert('Error Logging In');
+            });
 
 
     }
 
+    clearPopulateData();
+    loadUserData();
 
 }])
